6ad2a110233ddd388bc18cb97223b8c004abdb5a
Tino <tinomuzambi@gmail.com>
2020-08-04 15:21:51 +0200

    Adds necessary imports and modules.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-	return <div className="App"></div>;
+class App extends Component {
+	render() {
+		return (
+			<>
+				<h1>Blog.TinoMuzambi</h1>
+			</>
+		);
+	}
 }
 
 export default App;
