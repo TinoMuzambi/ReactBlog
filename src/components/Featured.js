8915d4bfefbd92e09ed068d73dfd18ab1742b7eb
Tino <tinomuzambi@gmail.com>
2020-08-09 09:59:51 +0200

    Adds HTML parsing to featured section.

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Iframe from "react-iframe";
 import video from "../data/featured-video";
 import Moment from "react-moment";
+import ReactHtmlParser from "react-html-parser";
 
 class Featured extends Component {
 	render() {
@@ -17,18 +18,18 @@ class Featured extends Component {
 						<h1 className="featured-title text-center">Featured Video</h1>
 						<div className="video-container">
 							<Iframe
-								url={video[0].url}
+								url={video.url}
 								id={0}
 								className="video"
 								allowFullScreen
 							/>
 						</div>
 
-						<h2>{video[0].title}</h2>
+						<h2>{video.title}</h2>
 						<h3>
 							Updated on: <Moment format="MMM DD, YYYY">{video.date}</Moment>
 						</h3>
-						<p>{video[0].description}</p>
+						<p>{ReactHtmlParser(video.description)}</p>
 					</div>
 				</div>
 			</>
--- src/data/featured-video.js
+++ src/data/featured-video.js
@@ -1,10 +1,7 @@
-const video = [
-	{
-		title: "I am Restored by Lecrae (Official Book Announcement)",
-		date: "Thursday 06 August 2020",
-		url: "https://www.youtube.com/embed/ELAhp0Fuhrc",
-		description: `In this video, Lecrae announces his upcoming book, I Am Restored over a dope music video.`,
-	},
-];
-
+const video = {
+	title: "I am Restored by Lecrae (Official Book Announcement)",
+	date: "Thursday 06 August 2020",
+	url: "https://www.youtube.com/embed/ELAhp0Fuhrc",
+	description: `<p>In this video, Lecrae announces his upcoming book, <i>I Am Restored</i> over a dope music video.</p>`,
+};
 export default video;
