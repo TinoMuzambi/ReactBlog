a13e9923cf26d91b4424579ed9ba7cc5aaa23356
Tino <tinomuzambi@gmail.com>
2020-08-14 20:13:41 +0200

    Updates featured item.

--- src/data/featured-item.js
+++ src/data/featured-item.js
@@ -1,7 +1,7 @@
 const item = {
-	title: "I am Restored by Lecrae (Official Book Announcement)",
-	date: "Thursday 06 August 2020",
-	url: "https://www.youtube.com/embed/ELAhp0Fuhrc",
-	description: `In this item, Lecrae announces his upcoming book, <i>I Am Restored</i> over a dope music item.`,
+	title: "The Road to Restoration (Official Trailer) | Aug. 14, 2020",
+	date: "Friday 14 August 2020",
+	url: "https://www.youtube.com/embed/DNL8SEZQpHE",
+	description: `<i>Restoration</i> is coming. August 21st.`,
 };
 export default item;
