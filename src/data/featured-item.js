495fe5a0bab089993aaf44c904a8489058bae74e
Tino <tinomuzambi@gmail.com>
2020-08-21 14:11:51 +0200

    Fixes youtube embed link.

--- src/data/featured-item.js
+++ src/data/featured-item.js
@@ -1,7 +1,7 @@
 const item = {
 	title: "Lecrae - Nothing Left To Hide feat. Gwen Bunn (Official Lyric Video)",
 	date: "Friday 21 August 2020",
-	url: "https://www.youtube.com/watch?v=gjMh2BrcRTk",
+	url: "https://www.youtube.com/embed/gjMh2BrcRTk",
 	description: `<i>Restoration</i> is here.`,
 };
 export default item;
