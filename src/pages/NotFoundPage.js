742940710ae83a164337e25eec29f30c31b4d9df
Tino <tinomuzambi@gmail.com>
2020-09-03 03:14:13 +0200

    Updates 404 scss.

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const NotFoundPage = () => (
 	<>
 		<div className="not-found-page">
-			<h1 className="not-found-title">Looks like you got lost.</h1>
+			<h1 className="title">Looks like you got lost.</h1>
 			<Link to="/">
-				<h2 className="not-found-subtitle">Let's get you back home.</h2>
+				<h2 className="subtitle">Let's get you back home.</h2>
 			</Link>
 		</div>
 	</>
