d312166f78e3ed0d9f5408f80562ce22ee86b9ee
Tino <tinomuzambi@gmail.com>
2020-10-26 13:38:34 +0200

    Adds helmet to not found page.

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const NotFoundPage = () => (
 	<>
+		<Helmet>
+			<title>Not Found | Blog.TinoMuzambi</title>
+			<meta name="Not Found" content="404 - Page not found" />
+			<meta itemprop="image" content="/logo512.png" />
+		</Helmet>
 		<div className="not-found-page">
 			<h1 className="title">Looks like you got lost.</h1>
 			<Link to="/">
