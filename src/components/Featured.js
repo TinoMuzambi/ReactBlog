b8aadeb80277fa59d2061cc7c49f18c35c3366cc
Tino <tinomuzambi@gmail.com>
2021-01-30 20:30:34 +0200

    Fixes player boolean.

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -23,7 +23,7 @@ class Featured extends Component {
 								className="item"
 								width="100%"
 								height="100%"
-								controls="true"
+								controls={true}
 							/>
 						</div>
 						<h2>{item.title}</h2>
