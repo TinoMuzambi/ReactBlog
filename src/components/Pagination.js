de5493612127801e83a4603317fd98c54f978899
Tino <tinomuzambi@gmail.com>
2021-02-21 00:21:04 +0200

    Adds some styling.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-const Pagination = ({ customLabels }) => {
+const Pagination = ({ pageSize, items, onChangePage, customLabels }) => {
+	console.log(Array.from(Array(items.length % pageSize).keys()));
 	return (
 		<div className="pagination">
-			{customLabels.previous}
-			{customLabels.next}
+			<ul>
+				<li>{customLabels.previous}</li>
+				{Array.from(Array(items.length % pageSize).keys()).map((i) => (
+					<li>{i + 1}</li>
+				))}
+				<li>{customLabels.next}</li>
+			</ul>
 		</div>
 	);
 };
