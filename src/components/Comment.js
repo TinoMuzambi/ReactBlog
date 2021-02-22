276c3d6accc6fe4f365d02e019b2bcbaf3e3c359
Tino <tinomuzambi@gmail.com>
2021-02-22 20:07:15 +0200

    Migrates to yarn.

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,7 +2,12 @@ import React from "react";
 
 const Comment = ({ comment }) => {
 	return (
-		<div>
+		<div className="comment">
+			<img src={comment.image} alt="Avatar" className="avatar" />
+			<div className="details">
+				<h4 className="author">{comment.user}</h4>
+				<h5 className="date">{comment.date.toLocaleString()}</h5>
+			</div>
 			<p>{comment.comment}</p>
 		</div>
 	);
