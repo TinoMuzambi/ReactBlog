076073c9a52229000607faa88b479a3a94aa125f
Tino <tinomuzambi@gmail.com>
2021-02-22 19:38:19 +0200

    Adds layout styling.

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -4,6 +4,15 @@ const Comments = () => {
 	return (
 		<div className="comments">
 			<h1>Comments</h1>
+			<div className="form-group">
+				<img
+					src="https://clinicforspecialchildren.org/wp-content/uploads/2016/08/avatar-placeholder.gif"
+					alt="Avatar"
+				/>
+				<form className="form">
+					<input type="text" name="text" className="text" />
+				</form>
+			</div>
 		</div>
 	);
 };
