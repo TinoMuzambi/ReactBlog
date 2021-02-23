91df59fae8ac75492f801b7b4d5116b0e102ee54
Tino <tinomuzambi@gmail.com>
2021-02-23 16:17:26 +0200

    Cleaning up.

--- src/components/CommentHolder.js
+++ src/components/CommentHolder.js
@@ -1,36 +1,8 @@
-import React, { useState } from "react";
-import { FcLike, FcLikePlaceholder } from "react-icons/fc";
-import Moment from "react-moment";
+import React from "react";
 
-import CommentForm from "./CommentForm";
 import CommentContent from "./CommentContent";
 
 const CommentHolder = ({ comments }) => {
-	const [replying, setReplying] = useState(false);
-	const [replying2, setReplying2] = useState(false);
-	const [replying3, setReplying3] = useState(false);
-
-	const like = (commentParam) => {
-		commentParam.liked = !commentParam.liked;
-		if (commentParam.liked) {
-			commentParam.upvotes++;
-		} else {
-			commentParam.upvotes--;
-		}
-	};
-
-	const replyHandler = (level) => {
-		if (level === 0) {
-			setReplying(!replying);
-		} else if (level === 1) {
-			setReplying2(!replying2);
-		} else {
-			setReplying3(!replying3);
-		}
-	};
-
-	// console.log(comments);
-
 	return comments.comments.map((comment) => (
 		<>
 			<CommentContent comment={comment} />
