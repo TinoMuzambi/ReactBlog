048223ba7171c45a69719ac68263da0201a59e76
Tino <tinomuzambi@gmail.com>
2021-02-24 11:51:43 +0200

    Temporarily bringing back the disk files.

---
+++ src/data/featured-item.js
@@ -0,0 +1,7 @@
+const item = {
+	title: "Would you sacrifice one person to save five? - Eleanor Nelsen",
+	date: "Thursday 31 Decemberr 2020",
+	url: "https://www.youtube.com/watch?v=yg16u_bzjPE",
+	description: `The infamous trolley problem. What would you do?`,
+};
+export default item;
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,5 +1,10 @@
 import StoryblokClient from "storyblok-js-client";
 
+// Temporary fetching from disk.
+import blogs from "../data/blogs";
+import categories from "../data/categories";
+import featured from "../data/featured-item";
+
 import { titleCase } from "./helpers";
 
 // const BASE_URL = "https://api.storyblok.com/v1/cdn";
@@ -20,74 +25,77 @@ const getSpaceVersion = async () => {
 };
 
 export const getBlogs = async () => {
-	await getSpaceVersion();
-	let prettyBlogs = [];
+	// await getSpaceVersion();
+	// let prettyBlogs = [];
 
-	await Storyblok.get("cdn/stories?starts_with=blogs/", {
-		sort_by: "content.date:desc",
-		cv: version,
-	})
-		.then((response) => {
-			const strictlyBlogs = response.data.stories;
-			prettyBlogs = strictlyBlogs.map((blog) => ({
-				category: titleCase(blog.content.category.cached_url.substring(11)),
-				content: blog.content.content,
-				date: blog.content.date,
-				disqusIdentifier: blog.content.disqusIdentifier,
-				disqusShortname: blog.content.disqusShortname,
-				disqusSrc: blog.content.disqusSrc,
-				disqusURL: blog.content.disqusURL,
-				future: blog.content.future,
-				image: blog.content.media.filename,
-				alt: blog.content.media.alt,
-				readTime: blog.content.readTime,
-				title: blog.content.title,
-				url: blog.content.url,
-				id: blog.content._uid,
-			}));
-		})
-		.catch((error) => {
-			console.error(error);
-		});
-	return prettyBlogs;
+	// await Storyblok.get("cdn/stories?starts_with=blogs/", {
+	// 	sort_by: "content.date:desc",
+	// 	cv: version,
+	// })
+	// 	.then((response) => {
+	// 		const strictlyBlogs = response.data.stories;
+	// 		prettyBlogs = strictlyBlogs.map((blog) => ({
+	// 			category: titleCase(blog.content.category.cached_url.substring(11)),
+	// 			content: blog.content.content,
+	// 			date: blog.content.date,
+	// 			disqusIdentifier: blog.content.disqusIdentifier,
+	// 			disqusShortname: blog.content.disqusShortname,
+	// 			disqusSrc: blog.content.disqusSrc,
+	// 			disqusURL: blog.content.disqusURL,
+	// 			future: blog.content.future,
+	// 			image: blog.content.media.filename,
+	// 			alt: blog.content.media.alt,
+	// 			readTime: blog.content.readTime,
+	// 			title: blog.content.title,
+	// 			url: blog.content.url,
+	// 			id: blog.content._uid,
+	// 		}));
+	// 	})
+	// 	.catch((error) => {
+	// 		console.error(error);
+	// 	});
+	// return prettyBlogs;
+	return blogs;
 };
 export const getCategories = async () => {
-	await getSpaceVersion();
-	let prettyCats = [];
-	await Storyblok.get("cdn/stories?starts_with=categories/", { cv: version })
-		.then((response) => {
-			const strictlyCats = response.data.stories;
-			prettyCats = strictlyCats.map((cat) => ({
-				count: cat.content.count,
-				alt: cat.content.image.alt,
-				image: cat.content.image.filename,
-				name: cat.content.name,
-				url: cat.content.url,
-				id: cat.content._uid,
-			}));
-		})
-		.catch((error) => {
-			console.error(error);
-		});
+	// await getSpaceVersion();
+	// let prettyCats = [];
+	// await Storyblok.get("cdn/stories?starts_with=categories/", { cv: version })
+	// 	.then((response) => {
+	// 		const strictlyCats = response.data.stories;
+	// 		prettyCats = strictlyCats.map((cat) => ({
+	// 			count: cat.content.count,
+	// 			alt: cat.content.image.alt,
+	// 			image: cat.content.image.filename,
+	// 			name: cat.content.name,
+	// 			url: cat.content.url,
+	// 			id: cat.content._uid,
+	// 		}));
+	// 	})
+	// 	.catch((error) => {
+	// 		console.error(error);
+	// 	});
 
-	return prettyCats;
+	// return prettyCats;
+	return categories;
 };
 export const getFeatured = async () => {
-	await getSpaceVersion();
-	let prettyFeat = {};
-	await Storyblok.get("cdn/stories/featured-item/", { cv: version })
-		.then((response) => {
-			const strictlyFeat = response.data.story.content;
-			prettyFeat = {
-				date: strictlyFeat.date,
-				description: strictlyFeat.description,
-				title: strictlyFeat.title,
-				url: strictlyFeat.url,
-			};
-		})
-		.catch((error) => {
-			console.error(error);
-		});
+	// await getSpaceVersion();
+	// let prettyFeat = {};
+	// await Storyblok.get("cdn/stories/featured-item/", { cv: version })
+	// 	.then((response) => {
+	// 		const strictlyFeat = response.data.story.content;
+	// 		prettyFeat = {
+	// 			date: strictlyFeat.date,
+	// 			description: strictlyFeat.description,
+	// 			title: strictlyFeat.title,
+	// 			url: strictlyFeat.url,
+	// 		};
+	// 	})
+	// 	.catch((error) => {
+	// 		console.error(error);
+	// 	});
 
-	return prettyFeat;
+	// return prettyFeat;
+	return featured;
 };
