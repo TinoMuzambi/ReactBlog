5a6fffdaa4027a44c85696a3ca868dc6064dbdad
Tino <tinomuzambi@gmail.com>
2021-02-28 13:29:04 +0200

    Adds logic for replacing deleted comment with deleting.

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -5,60 +5,70 @@ const Comment = ({ commentProp }) => {
 	const [currComments, setCurrComments] = useState(commentProp);
 
 	const deleteHandler = (id) => {
-		console.log(id);
-		console.log("before", currComments);
-		const topIDs = currComments.comments.map((comment) => comment.id);
-		const secondIDs = currComments.comments.map((comment) =>
-			comment.replies.map((reply) => reply.id)
-		)[0];
-		// const thirdIDs = currComments.comments.map(
-		// 	(comment) =>
-		// 		comment.replies.map((reply) =>
-		// 			reply.replies.map((replyTwo) => replyTwo.id)
-		// 		)[0]
-		// );
-		if (topIDs.includes(id)) {
-			let newComments = [];
-			for (let i = 0; i < currComments?.comments?.length; i++) {
-				if (currComments?.comments[i]?.id === id) {
-					newComments = currComments?.comments[i]?.replies;
-					for (let j = 0; j < newComments?.length; j++) {
-						newComments[j].level = "zero";
-						for (let k = 0; k < newComments[j]?.replies?.length; k++) {
-							newComments[j].replies[k].level = "one";
-						}
+		if (window.confirm("Are you sure you finna delete?")) {
+			console.log(id);
+			console.log("before", currComments);
+			const topIDs = currComments.comments.map((comment) => comment.id);
+			const secondIDs = currComments.comments.map((comment) =>
+				comment.replies.map((reply) => reply.id)
+			)[0];
+			// const thirdIDs = currComments.comments.map(
+			// 	(comment) =>
+			// 		comment.replies.map((reply) =>
+			// 			reply.replies.map((replyTwo) => replyTwo.id)
+			// 		)[0]
+			// );
+			if (topIDs.includes(id)) {
+				let newComments = [];
+				for (let i = 0; i < currComments?.comments?.length; i++) {
+					if (currComments?.comments[i]?.id === id) {
+						newComments.push({
+							...currComments?.comments[i],
+							comment: "deleted",
+						});
+						// for (let j = 0; j < newComments?.length; j++) {
+						// 	newComments[j].level = "zero";
+						// 	for (let k = 0; k < newComments[j]?.replies?.length; k++) {
+						// 		newComments[j].replies[k].level = "one";
+						// 	}
+						// }
 					}
 				}
-			}
-			const newItem = {
-				blog_url: currComments.blog_url,
-				comments: newComments,
-			};
-			setCurrComments(newItem);
-		} else if (secondIDs.includes(id)) {
-			let newComments = currComments?.comments;
-			for (let i = 0; i < currComments?.comments[i]?.replies?.length; i++) {
-				newComments[i].replies = currComments?.comments[i]?.replies[i]?.replies;
-				for (let j = 0; j < newComments[i]?.replies?.length; j++) {
-					newComments[i].replies[j].level = "one";
+				// console.log(newComments);
+				const newItem = {
+					blog_url: currComments.blog_url,
+					comments: newComments,
+				};
+				console.log(newItem);
+				setCurrComments(newItem);
+			} else if (secondIDs.includes(id)) {
+				let newComments = currComments?.comments;
+				for (let i = 0; i < currComments?.comments[i]?.replies?.length; i++) {
+					newComments[i].replies =
+						currComments?.comments[i]?.replies[i]?.replies;
+					for (let j = 0; j < newComments[i]?.replies?.length; j++) {
+						newComments[i].replies[j].level = "one";
+					}
 				}
+				const newItem = {
+					blog_url: currComments.blog_url,
+					comments: newComments,
+				};
+				setCurrComments(newItem);
+			} else {
+				// const newComments = currComments.comments.map((comment) =>
+				// 	comment.replies.map((reply) =>
+				// 		reply.replies.filter((replyTwo) => replyTwo.id !== id)
+				// 	)
+				// );
+				// const newItem = {
+				// 	blog_url: currComments.blog_url,
+				// 	comments: newComments,
+				// };
+				// setCurrComments(newItem);
 			}
-			const newItem = {
-				blog_url: currComments.blog_url,
-				comments: newComments,
-			};
-			setCurrComments(newItem);
 		} else {
-			// const newComments = currComments.comments.map((comment) =>
-			// 	comment.replies.map((reply) =>
-			// 		reply.replies.filter((replyTwo) => replyTwo.id !== id)
-			// 	)
-			// );
-			// const newItem = {
-			// 	blog_url: currComments.blog_url,
-			// 	comments: newComments,
-			// };
-			// setCurrComments(newItem);
+			console.log("Aight");
 		}
 	};
 
