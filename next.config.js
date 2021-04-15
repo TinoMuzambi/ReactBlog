58781271a32c7fa13f7369cd6004d8ddc0f8fa2b
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-15 22:18:11 +0200

    Fixes env.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 module.exports = {
-	webpack: (config, { isServer }) => {
-		if (isServer) {
-			require("./utils/generateSiteMap");
-		}
-
-		return config;
-	},
 	env: {
 		REACT_APP_CONTENTFUL_KEY: process.env.REACT_APP_CONTENTFUL_KEY,
 		REACT_APP_STORYBLOK_KEY: process.env.REACT_APP_STORYBLOK_KEY,
-		REACT_APP_CONTENTFUL_KEY: process.env.REACT_APP_CONTENTFUL_KEY,
+		REACT_APP_FIREBASE_KEY: process.env.REACT_APP_FIREBASE_KEY,
 	},
 };
