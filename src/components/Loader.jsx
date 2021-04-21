ed8e5091ee9a7d3a1817357ba17d967ffbb87267
Tino Muzambi <tinomuzambi@gmail.com>
2021-04-21 19:56:05 +0200

    Styles initial look of loader.

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,28 +4,38 @@ const Loader = () => {
 	return (
 		<div className="loader">
 			<div className="box">
-				<div className="profile">
+				<div className="profile"></div>
+				<div className="bars">
 					<div className="bar"></div>
+					<div className="bar sm"></div>
 				</div>
 			</div>
 			<div className="box">
-				<div className="profile">
+				<div className="profile"></div>
+				<div className="bars">
 					<div className="bar"></div>
+					<div className="bar sm"></div>
 				</div>
 			</div>
 			<div className="box">
-				<div className="profile">
+				<div className="profile"></div>
+				<div className="bars">
 					<div className="bar"></div>
+					<div className="bar sm"></div>
 				</div>
 			</div>
 			<div className="box">
-				<div className="profile">
+				<div className="profile"></div>
+				<div className="bars">
 					<div className="bar"></div>
+					<div className="bar sm"></div>
 				</div>
 			</div>
 			<div className="box">
-				<div className="profile">
+				<div className="profile"></div>
+				<div className="bars">
 					<div className="bar"></div>
+					<div className="bar sm"></div>
 				</div>
 			</div>
 		</div>
