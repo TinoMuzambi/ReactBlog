202f0a26ab43819c7b9272bc9579db368c57a254
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-05 18:03:33 +0200

    Adds sass logic for showing title on hover.

--- src/App.jsx
+++ src/App.jsx
@@ -126,7 +126,7 @@ const App = () => {
 			/>
 			<button
 				className="dark-toggle"
-				title={dark ? "Switch to light mode" : "Switch to dark mode"}
+				// title={dark ? "Switch to light mode" : "Switch to dark mode"}
 				onClick={() => {
 					setDark(!dark);
 
