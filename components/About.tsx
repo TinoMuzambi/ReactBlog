f64606c20540a9ea92b820b8f8839f734915731e
Tino Muzambi <tinomuzambi@gmail.com>
2022-04-13 20:28:42 +0200

    Fixes image.

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,11 @@
 import { blokProps } from "../interfaces";
+import DynamicComponent from "./DynamicComponent";
 
 const About: React.FC<blokProps> = ({ blok }): JSX.Element => {
 	return (
 		<section className="site-title">
 			<div className="site-background text-center">
+				<DynamicComponent blok={blok.image} />
 				<h1>{blok.title}</h1>
 				<h2>
 					<s>{blok.fake_job_title}</s>&nbsp;{blok.job_title}
--- components/Image.tsx
+++ components/Image.tsx
@@ -1,6 +1,6 @@
 import { blokProps } from "../interfaces";
 
 const Image: React.FC<blokProps> = ({ blok }): JSX.Element => {
-	return <img src={blok.image.filename} alt={blok.image.alt} />;
+	return <img src={blok.filename} alt={blok.alt} />;
 };
 export default Image;
