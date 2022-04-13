d453aa3a4dc6ae0ff6aeeb1ee3f893b701b1d8c6
Tino Muzambi <tinomuzambi@gmail.com>
2022-04-13 19:43:12 +0200

    App setup.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,16 @@
+import { useState, useEffect } from "react";
+import { AiOutlineReload } from "react-icons/ai";
+import { RiLightbulbFlashLine, RiLightbulbFill } from "react-icons/ri";
+import AOS from "aos";
+
 const Home: React.FC = (): JSX.Element => {
+	const [queryText, setQueryText] = useState("");
+	const [searching, setSearching] = useState(false);
+	const [dark, setDark] = useState(false);
+	const [fetching, setFetching] = useState(true);
+	const [blogs, setBlogs] = useState([]);
+	const [categories, setCategories] = useState([]);
+	const [featuredItem, setFeaturedItem] = useState({});
 	return (
 		<main>
 			<p>Clean Next.js with TypeScript and Sass Boilerplate</p>
