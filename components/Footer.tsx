6d2e5be7ef3ee43bfca241c5c4c5b70c8184affa
Tino Muzambi <tinomuzambi@gmail.com>
2022-04-14 22:48:48 +0200

    Adds scroll to top button.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
+import { FaArrowCircleUp } from "react-icons/fa";
+
 import { blokProps } from "../interfaces";
 import DynamicComponent from "./DynamicComponent";
 
 const Footer: React.FC<blokProps> = ({ blok }): JSX.Element => {
+	const scrollTop = () => {
+		// Scroll to top handler.
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	};
 	return (
 		<section className="footer" id="footer">
 			<footer className="footer">
@@ -10,6 +16,13 @@ const Footer: React.FC<blokProps> = ({ blok }): JSX.Element => {
 				</div>
 				<div className="rights flex-row text-center">
 					<h4 className="text-gray">{blok.copy}</h4>
+				</div>
+				<div className="move-up">
+					<span onClick={scrollTop}>
+						<i className="fas fa-arrow-circle-up fa-2x">
+							<FaArrowCircleUp />
+						</i>
+					</span>
 				</div>
 			</footer>
 		</section>
