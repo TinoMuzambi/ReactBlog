ddd12bd1738a8ed4194c9119863b383f7faf27b7
Tino Muzambi <tinomuzambi@gmail.com>
2022-04-14 22:23:59 +0200

    Fixes sidebar.

--- components/SideBlog.tsx
+++ components/SideBlog.tsx
@@ -3,60 +3,52 @@ import { FaCalendar } from "react-icons/fa";
 import Moment from "react-moment";
 
 import { blokProps } from "../interfaces";
+import DynamicComponent from "./DynamicComponent";
 
 const SideBlog: React.FC<blokProps> = ({ blok }): JSX.Element => {
+	console.log(blok);
 	return (
-		<>
-			<h2>{blok.future ? "Future" : "Other"} Blogs</h2>
-			{blok.other_blogs.map((blog: any, key: number) => (
-				<div
-					className="post-content"
-					data-aos="flip-up"
-					data-aos-delay="200"
-					key={key}
-				>
-					<div className="post-image">
-						<div>
-							{/* If future just show image, else show image with link to that blog. */}
-							{blok.future ? (
-								<img src={blog.image} className="img" alt={blog.alt} />
-							) : (
-								<Link href={`/blogs/${blog.url}`}>
-									<a>
-										<img src={blog.image} className="img" alt={blog.alt} />
-									</a>
-								</Link>
-							)}
-						</div>
-						<div className="post-info flex-row">
-							<span>
-								<i className="fas fa-calendar-alt text-gray">
-									<FaCalendar />
-								</i>
-								&nbsp;&nbsp;
-								{/* If future, show TBA for date, else show published date.*/}
-								{blok.future ? (
-									"TBA"
-								) : (
-									<Moment format="MMMM DD, YYYY">{blog.date}</Moment>
-								)}
-							</span>
-						</div>
-					</div>
-
-					<div className="post-title">
-						{/* If future show title with future tag, else show link to that blog. */}
-						{blok.future ? (
-							<h3 className="side-future">{blog.title}</h3>
-						) : (
-							<Link href={`/blogs/${blog.url}`}>
-								<a>{blog.title}</a>
-							</Link>
-						)}
-					</div>
+		<div className="post-content" data-aos="flip-up" data-aos-delay="200">
+			<div className="post-image">
+				<div>
+					{/* If future just show image, else show image with link to that blog. */}
+					{blok.future ? (
+						<DynamicComponent blok={blok.image[0]} />
+					) : (
+						<Link href={`/blogs/${blok.url}`}>
+							<a>
+								<DynamicComponent blok={blok.image[0]} />
+							</a>
+						</Link>
+					)}
 				</div>
-			))}
-		</>
+				<div className="post-info flex-row">
+					<span>
+						<i className="fas fa-calendar-alt text-gray">
+							<FaCalendar />
+						</i>
+						&nbsp;&nbsp;
+						{/* If future, show TBA for date, else show published date.*/}
+						{blok.future ? (
+							"TBA"
+						) : (
+							<Moment format="MMMM DD, YYYY">{blok.date}</Moment>
+						)}
+					</span>
+				</div>
+			</div>
+
+			<div className="post-title">
+				{/* If future show title with future tag, else show link to that blog. */}
+				{blok.future ? (
+					<h3 className="side-future">{blok.title}</h3>
+				) : (
+					<Link href={`/blogs/${blok.url}`}>
+						<a>{blok.title}</a>
+					</Link>
+				)}
+			</div>
+		</div>
 	);
 };
 export default SideBlog;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,14 +2,16 @@ import { blokProps } from "../interfaces";
 import DynamicComponent from "./DynamicComponent";
 
 const Sidebar: React.FC<blokProps> = ({ blok }): JSX.Element => {
-	console.log(blok);
 	return (
 		<aside className="sidebar">
 			<div className="category">
 				<DynamicComponent blok={blok.categories[0]} />
 			</div>
 			<div className="other-posts">
-				<DynamicComponent blok={blok.other_blogs[0]} />
+				<h2>{blok.type === "future" ? "Future" : "Other"} Blogs</h2>
+				{blok.other_blogs.map((blog: any, key: number) => (
+					<DynamicComponent blok={blog} key={key} />
+				))}
 			</div>
 		</aside>
 	);
