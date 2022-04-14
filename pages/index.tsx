edb71f729a440b5a4d46d9176d3b3b20f815490d
Tino Muzambi <tinomuzambi@gmail.com>
2022-04-14 22:48:06 +0200

    Adds dark mode.

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,47 @@ import Page from "../components/Page";
 
 const Home: React.FC<HomeProps> = ({ story }): JSX.Element => {
 	const storyblokUser = useStoryblok(story);
+	const [dark, setDark] = useState(false);
+
+	useEffect(() => {
+		AOS.init(); // Initialise animate on scroll library.
+	}, []);
+
+	useEffect(() => {
+		const lsDark = JSON.parse(localStorage.getItem("blogtino-dark") as string);
+		if (lsDark === true || lsDark === false) {
+			setDark(lsDark);
+			document.body.classList.remove("dark");
+
+			lsDark
+				? document.body.classList.add("dark")
+				: document.body.classList.remove("dark");
+		}
+	}, []);
 
 	return (
 		<main className="home">
+			<button
+				className="dark-toggle"
+				data-mode={dark ? "Switch to light mode" : "Switch to dark mode"}
+				onClick={() => {
+					setDark(!dark);
+
+					document.body.classList.remove("dark");
+
+					!dark
+						? document.body.classList.add("dark")
+						: document.body.classList.remove("dark");
+
+					localStorage.setItem("blogtino-dark", JSON.stringify(!dark));
+				}}
+			>
+				{dark ? (
+					<RiLightbulbFlashLine className="icon" />
+				) : (
+					<RiLightbulbFill className="icon" />
+				)}
+			</button>
 			<Page content={storyblokUser?.content} />
 		</main>
 	);
