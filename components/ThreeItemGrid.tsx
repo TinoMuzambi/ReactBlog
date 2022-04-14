1703f6f78f6092d3e2a61c9c7888536bd2da0de1
Tino Muzambi <tinomuzambi@gmail.com>
2022-04-14 21:36:11 +0200

    Removes three grid classes.

--- components/ThreeItemGrid.tsx
+++ components/ThreeItemGrid.tsx
@@ -3,11 +3,11 @@ import DynamicComponent from "./DynamicComponent";
 
 const ThreeItemGrid: React.FC<blokProps> = ({ blok }) => {
 	return (
-		<section className="three-grid">
+		<>
 			{blok.columns.map((blok: any, key: any) => (
 				<DynamicComponent key={key} blok={blok} />
 			))}
-		</section>
+		</>
 	);
 };
 
