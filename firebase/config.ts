26285fa2e476c8433bb95f90d71860d0c9254c5c
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-21 15:45:45 +0200

    Adds firebase deps.

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,10 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+// import { seedDatabase } from "./seed";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_KEY,
 	authDomain: "blogtinomuzambi-6b696.firebaseapp.com",
 	projectId: "blogtinomuzambi-6b696",
@@ -12,8 +11,8 @@ const firebaseConfig = {
 	messagingSenderId: "327039704768",
 	appId: "1:327039704768:web:a310158b4ab1c534411725",
 };
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export default app;
+// seedDatabase(firebaseIn);
+export { firebase };
