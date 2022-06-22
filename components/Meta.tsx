c933145cca85d805267f48290e769ac2e276e2e0
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-22 11:48:25 +0200

    Updates meta.

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -14,29 +14,29 @@ const Meta: React.FC<MetaProps> = ({
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="theme-color" content="#000000" />
 		<meta name="keywords" content={keywords} />
-		<meta name="description" content={description} />
+		<meta name="description" content={description?.substring(0, 300)} />
 
 		{/* <!-- Google / Search Engine Tags --> */}
 		<meta itemProp="name" content={title} />
-		<meta itemProp="description" content={description} />
+		<meta itemProp="description" content={description?.substring(0, 300)} />
 		<meta itemProp="image" content={image} />
 
 		{/* <!-- Facebook Meta Tags --> */}
 		<meta property="og:url" content={url} />
 		<meta property="og:type" content="website" />
 		<meta property="og:title" content={title} />
-		<meta property="og:description" content={description} />
+		<meta property="og:description" content={description?.substring(0, 300)} />
 		<meta property="og:image" content={image} />
 
 		{/* <!-- Twitter Meta Tags --> */}
 		<meta name="twitter:card" content="summary_large_image" />
 		<meta name="twitter:title" content={title} />
-		<meta name="twitter:description" content={description} />
+		<meta name="twitter:description" content={description?.substring(0, 200)} />
 		<meta name="twitter:image" content={image} />
 
 		<meta charSet="utf-8" />
 		<link rel="icon" href="/favicon.ico" />
-		<meta name={title} content={description} />
+		<meta name={title} content={description?.substring(0, 300)} />
 		<link rel="apple-touch-icon" href="/logo192.png" />
 		<link rel="manifest" href="/manifest.json" />
 		<title>{title}</title>
@@ -50,7 +50,8 @@ const Meta: React.FC<MetaProps> = ({
 Meta.defaultProps = {
 	title: "Blog.TinoMuzambi",
 	keywords: "blog, lifestyle, tech",
-	description: "Blog.TinoMuzambi",
+	description:
+		"Welcome to Blog.TinoMuzambi. All things tech, lifestyle and random.",
 	image: "https://a.storyblok.com/f/105639/512x512/03489159d5/logo512.png",
 	url: BASE_URL,
 };
