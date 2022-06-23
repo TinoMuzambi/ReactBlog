ac006d7f3f6af5e9d14159382aee314471067a66
Tinotenda Muzambi <tinomuzambi@gmail.com>
2022-06-23 21:04:34 +0200

    Adds scripts for ga.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,23 @@ export default class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Abel&family=Anton&family=Courier+Prime:wght@400;700&family=Josefin+Sans:wght@300;400;500;600;700&family=Lexend+Deca:wght@100;200;300;400;500;600;700;800&family=Livvic:wght@100;200;300;400;500;600;700;900&display=swap"
 						rel="stylesheet"
 					/>
+					{/* Global Site Tag (gtag.js) - Google Analytics */}
+					<script
+						async
+						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.ANALYTICS_CODE}`}
+					/>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.ANALYTICS_CODE}', {
+              page_path: window.location.pathname,
+            });
+          `,
+						}}
+					/>
 				</Head>
 				<body>
 					<Main />
