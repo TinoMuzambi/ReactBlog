000a08240f6bd24ecff1a04b98e0466c329c50b1
Tino Muzambi <tinomuzambi@gmail.com>
2022-06-24 11:26:16 +0200

    Moves pagination in house.

--- components/Blogs.tsx
+++ components/Blogs.tsx
@@ -2,23 +2,57 @@ import { useEffect, useRef, useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 import { blokProps } from "../interfaces";
+import { getNoItemsOnPage, useDidMountEffect } from "../utils";
+import { executeScroll } from "../utils/helpers";
 import DynamicComponent from "./DynamicComponent";
-import Pagination from "./Pagination";
 
 const Blogs: React.FC<blokProps> = ({ blok }): JSX.Element => {
-	const [displayBlogs, setDisplayBlogs] = useState(blok.blogs);
-	const [blogItems, setBlogItems] = useState(blok.blogs);
+	const [startPos, setStartPos] = useState(0);
+	const [endPos, setEndPos] = useState(0);
+	const [currPage, setCurrPage] = useState(0);
 
-	const handlePageChange = (paginatedBlogs: any[]) => {
+	const noPages = Math.ceil(blok.blogs.length / 3);
+
+	const handlePageChange = (start: number, end: number) => {
 		// Handing pagination page changes.
-		console.log(paginatedBlogs);
-		setDisplayBlogs(paginatedBlogs);
+		setStartPos(start);
+		setEndPos(end);
+		console.log({ start, end });
 	};
 
-	useEffect(() => {
-		setBlogItems(blok.blogs);
-		setDisplayBlogs(blok.blogs);
-	}, [blok.blogs]);
+	useDidMountEffect(() => {
+		const noItems = getNoItemsOnPage(blok.blogs.length, 3, currPage);
+
+		handlePageChange(currPage * 3, currPage * 3 + noItems);
+	}, [blok.blogs.length, currPage]);
+
+	/**
+	 * Switch to next page of items check against going past last page.
+	 */
+	const nextPage = () => {
+		setCurrPage(currPage + 1);
+		if (currPage >= noPages - 1) {
+			setCurrPage(noPages - 1);
+		}
+	};
+
+	/**
+	 * Switch to previous page of items and check against going before first page.
+	 */
+	const prevPage = () => {
+		setCurrPage(currPage - 1);
+		if (currPage <= 0) {
+			setCurrPage(0);
+		}
+	};
+
+	/**
+	 * Directly switch to clicked on page.
+	 * @param page The page to switch to.
+	 */
+	const setDirectPage = (page: number) => {
+		setCurrPage(page);
+	};
 
 	const customLabels = {
 		// Custom labels for pagination.
@@ -33,27 +67,35 @@ const Blogs: React.FC<blokProps> = ({ blok }): JSX.Element => {
 			<section className="container" id="blogs">
 				<section className="site-content">
 					<section className="blogs">
-						<div className="posts">
+						<div className="posts" ref={ref}>
 							<h1>{blok.title}</h1>
-							{displayBlogs.length ? (
+							{blok.blogs.length ? (
 								<>
-									{displayBlogs
+									{blok.blogs
 										.sort((a: any, b: any) =>
 											b.content?.date.localeCompare(a.content?.date)
 										)
+										.slice(startPos, endPos)
 										.map((blog: any, key: number) => (
 											<DynamicComponent blok={blog.content} key={key} />
 										))}
 									<div className="page-holder text-center">
 										{/* Pagination element */}
-										{blogItems.length && (
-											<Pagination
-												items={blok.blogs}
-												onChangePage={handlePageChange}
-												pageSize={3}
-												customLabels={customLabels}
-												customRef={ref}
-											/>
+										{blok.blogs.length && (
+											<div
+												className="pagination"
+												onClick={() => executeScroll(ref)}
+											>
+												<ul>
+													<li onClick={prevPage}>{customLabels.previous}</li>
+													{Array.from(Array(noPages).keys()).map((i) => (
+														<li key={i} onClick={() => setDirectPage(i)}>
+															{i + 1}
+														</li>
+													))}
+													<li onClick={nextPage}>{customLabels.next}</li>
+												</ul>
+											</div>
 										)}
 									</div>
 								</>
--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,52 @@
+import { useEffect, useRef } from "react";
+
 export const BASE_URL =
 	process.env.NODE_ENV === "production"
 		? "https://blog.tinomuzambi,com"
 		: "http://localhost:3000";
+
+/**
+ * Computes the number of items that should be visible on a given page.
+ * @param length The length of the items.
+ * @param perPage The number of items that should be visible per page.
+ * @param page The current page.
+ * @returns The number of items that should be on the current page
+ */
+export const getNoItemsOnPage = (
+	length: number,
+	perPage: number,
+	page: number
+) => {
+	let globalCount = 1;
+	for (let i = 0; i < length; i++) {
+		let count = 0;
+		for (let j = 0; j < length; j++) {
+			count++;
+			if ((count === perPage && page === i) || globalCount === length) {
+				return count;
+			}
+			if (globalCount === perPage) {
+				globalCount++;
+				count = 0;
+			} else {
+				globalCount++;
+			}
+		}
+	}
+	return 0;
+};
+
+/**
+ * Custom hook to only run the callback function after the initial render.
+ * @param func The callback function to run
+ * @param deps The dependecy array for useEffect
+ */
+export const useDidMountEffect = (func: Function, deps: any[]) => {
+	const didMount = useRef(false);
+
+	useEffect(() => {
+		if (didMount.current) func();
+		else didMount.current = true;
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, deps);
+};
