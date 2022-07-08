598092f5d5a3dafc5c7d93d2ed9b3dface670d58
Tino Muzambi <tinomuzambi@gmail.com>
2022-07-08 10:40:29 +0200

    Check which type of date it is.

--- components/Comments/CommentContent.jsx
+++ components/Comments/CommentContent.jsx
@@ -408,9 +408,19 @@ const CommentContent /*: React.FC<CommentContentProps>*/ = ({
 							<div className="group">
 								<div className="details">
 									<h4 className="author">{comment.user}</h4>
-									<h5 className="date" data-date={comment.date.toDate()}>
+
+									<h5
+										className="date"
+										data-date={
+											comment.date.seconds
+												? comment.date.toDate()
+												: comment.date
+										}
+									>
 										<Moment format="MMM DD, YYYY">
-											{comment.date.toDate()}
+											{comment.date.seconds
+												? comment.date.toDate()
+												: comment.date}
 										</Moment>
 									</h5>
 								</div>
